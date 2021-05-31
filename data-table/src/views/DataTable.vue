1c67329f8b9825778ef49b46fd00d6560ec69e26
Neelima Nandam <neelimanandam@gmail.com>
2021-05-31 15:50:47 -0400

    created new data-table view

---
+++ data-table/src/views/DataTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <data-table></data-table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DataTable from '@/components/DataTable.vue'
+
+export default {
+  name: 'DataTable',
+  components: {
+    DataTable
+  }
+}
+</script>
